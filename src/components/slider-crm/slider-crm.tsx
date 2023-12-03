1ecd7599872064e72bf04b055f17a9558c2a563b
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2023-12-03 03:36:09 +0200

    vbcvbcb

--- src/components/slider-crm/slider-crm.tsx
+++ src/components/slider-crm/slider-crm.tsx
@@ -15,73 +15,71 @@ export interface ISliderCrmDataOnScreen {
 }
 
 export const SliderCrm = ({ slides }: ISliderCrmDataOnScreen) => {
-  //   const wrapperRef = useRef<any>(null);
+  const wrapperRef = useRef<any>(null);
 
-  //   const [swiperIsEnd, setSwiperIsEnd] = useState(false);
-  //   const [swiperIsBeginning, setSwiperIsBeginning] = useState(false);
-  //   const [isBlockInCenter, setIsBlockInCenter] = useState(false);
+  const [swiperIsEnd, setSwiperIsEnd] = useState(false);
+  const [swiperIsBeginning, setSwiperIsBeginning] = useState(false);
+  const [isBlockInCenter, setIsBlockInCenter] = useState(false);
 
-  //   const handleSlideChange = (swiper: any) => {
-  //     setSwiperIsEnd(swiper.realIndex === slides.length - 1);
-  //     setSwiperIsBeginning(swiper.realIndex === 0);
-  //   };
+  const handleSlideChange = (swiper: any) => {
+    setSwiperIsEnd(swiper.realIndex === slides.length - 1);
+    setSwiperIsBeginning(swiper.realIndex === 0);
+  };
 
-  //   useEffect(() => {
-  //     const handleScroll = () => {
-  //       const rect = wrapperRef.current.getBoundingClientRect();
-  //       const isCenter =
-  //         window.innerHeight / 2 >= rect.top &&
-  //         window.innerHeight / 2 <= rect.bottom;
-  //       console.log(isCenter);
+  useEffect(() => {
+    const handleScroll = () => {
+      const rect = wrapperRef.current.getBoundingClientRect();
+      const isCenter =
+        window.innerHeight / 2 >= rect.top &&
+        window.innerHeight / 2 <= rect.bottom;
+      console.log(isCenter);
 
-  //       setIsBlockInCenter(isCenter);
-  //     };
+      setIsBlockInCenter(isCenter);
+    };
 
-  //     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-  //     return () => window.removeEventListener("scroll", handleScroll);
-  //   }, [isBlockInCenter, swiperIsEnd, swiperIsBeginning]);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [isBlockInCenter, swiperIsEnd, swiperIsBeginning]);
 
-  //   useEffect(() => {
-  //     if (!isBlockInCenter) {
-  //       document.body.style.overflow = "auto";
-  //       return;
-  //     }
+  useEffect(() => {
+    if (!isBlockInCenter) {
+      document.body.style.overflow = "auto";
+      return;
+    }
 
-  //     if (swiperIsEnd) {
-  //       window.scrollTo({ top: window.scrollY + 10 });
-  //       document.body.style.overflow = "auto";
-  //     } else if (swiperIsBeginning) {
-  //       window.scrollTo({ top: window.scrollY - 10 });
-  //       document.body.style.overflow = "auto";
-  //     }
-  //     setTimeout(() => {
-  //       const rect = wrapperRef.current.getBoundingClientRect();
-  //       const centerY = window.innerHeight / 2;
-  //       console.log(centerY);
-  //       console.log(rect.top);
-  //       console.log(rect.bottom);
+    if (swiperIsEnd) {
+      document.body.style.overflow = "auto";
+    } else if (swiperIsBeginning) {
+      document.body.style.overflow = "auto";
+    }
+    setTimeout(() => {
+      const rect = wrapperRef.current.getBoundingClientRect();
+      const centerY = window.innerHeight / 2;
+      console.log(centerY);
+      console.log(rect.top);
+      console.log(rect.bottom);
 
-  //       const isCenter =
-  //         window.innerHeight / 2 >= rect.top &&
-  //         window.innerHeight / 2 <= rect.bottom;
+      const isCenter =
+        window.innerHeight / 2 >= rect.top &&
+        window.innerHeight / 2 <= rect.bottom;
 
-  //       if (isCenter) {
-  //         document.body.style.overflow = "hidden";
-  //       } else {
-  //         document.body.style.overflow = "auto";
-  //       }
-  //     }, 100);
-  //   }, [isBlockInCenter, swiperIsEnd, swiperIsBeginning]);
+      if (isCenter) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "auto";
+      }
+    }, 100);
+  }, [isBlockInCenter, swiperIsEnd, swiperIsBeginning]);
 
   return (
-    <div className="slider-crm-wrapper">
+    <div ref={wrapperRef} className="slider-crm-wrapper">
       <div className="container">
         <Swiper
           modules={[Mousewheel, Pagination]}
           slidesPerView={1}
           freeMode={true}
-          //   onSlideChange={handleSlideChange}
+          onSlideChange={handleSlideChange}
           mousewheel={{
             releaseOnEdges: true,
           }}
